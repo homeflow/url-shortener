b1ea91a3eda2173ee104696fdf28dc652b01b2a4
Felipe Ribeiro <felipernb@gmail.com>
2011-12-12 11:43:58 -0300

    adding click counters

--- app.rb
+++ app.rb
@@ -5,17 +5,35 @@ require './token_generator'
 require 'hiredis'
 require 'redis'
 require 'uri'
+require './page'
 
 before do
     uri = URI.parse(ENV["REDIS_URL"])
     $r = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) if !$r
 end
 
-get "/" do
-    "URL Shortener"
+get "/urls" do
+    H = HTMLGen.new if !defined?(H)
+    H.set_title "URLs"
+    url_list = ""
+    $r.smembers('urls').each do |token_url| 
+        token, url = token_url.split('*')
+        clicks = $r.get(CLICK_COUNTER_NAMESPACE + token)
+        clicks = 0 if clicks.nil?
+        url_list << H.li {DOMAIN + token + " - " + url + ": "+ clicks + " clicks"}
+    end
+    H.page {
+        H.ul {
+            url_list
+        }
+    }
 end
 
-get %r{/([\w]+)} do |hash|
+get %r{/([a-zA-Z0-9]+)/stats} do |hash|
+    $r.get(CLICK_COUNTER_NAMESPACE + hash)
+end
+
+get %r{/([a-zA-Z0-9]+)} do |hash|
 	url = $r.get(URL_TOKEN_NAMESPACE + hash)
 	unless url.nil?
 		$r.incr(CLICK_COUNTER_NAMESPACE + hash)
@@ -31,7 +49,7 @@ post "/url" do
 	unless params[:url].nil? and (params[:url] =~ URI::regexp).nil?
 		token = TokenGenerator.new.generate_token(params[:url])
 		$r.set(URL_TOKEN_NAMESPACE + token, params[:url])
-        $r.lpush('urls',params[:url])
+        $r.sadd('urls',token+ "*" +params[:url])
         response = {'shorturl' => DOMAIN + token}
 	else
 		status 400 # Bad Request
